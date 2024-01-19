e255b28faae81bbf7290b6fd1588bc93b85db4ed
Raghuvenkatasamy <raghukvr905@gmail.com>
2024-01-19 15:47:33 +0530

    save

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the start date");
+            int startdate = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the end date");
+            var enddate = Convert.ToInt32(Console.ReadLine());
+            var year = (startdate + enddate) / 365;
+            var weeks = (startdate + enddate) / 7;
+            Console.WriteLine($"{year}-years,{weeks}-weeks");
+        }
+    }
+}
