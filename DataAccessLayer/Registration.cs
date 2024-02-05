12c06f3d0c22dbc259864858382b9aaddfb20507
Raghuvenkatasamy <raghukvr905@gmail.com>
2024-02-05 07:10:42 +0530

    do

--- DataAccessLayer/Registration.cs
+++ DataAccessLayer/Registration.cs
@@ -13,13 +13,10 @@ namespace DataAccessLayer
         [Required(ErrorMessage = "Please enter your name"), MaxLength(50)]
         [StringLength(50, ErrorMessage = "Please do not enter values over 50 characters")]
         public string UserName { get; set; }
-        [DataType(DataType.Password)]
         [Required(ErrorMessage = "Password is Required")]
         public string Password { get; set; }
-        [DataType(DataType.Password)]
         [Required(ErrorMessage = "Password is Required")]
         [Compare("Password", ErrorMessage = "Password and Confirm Password do not match")]
-
         public string ConformPassword { get; set; }
 
     }
